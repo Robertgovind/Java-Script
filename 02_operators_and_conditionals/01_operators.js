b5ae3feb2997b56533e5374832e1de2e21cfb99c
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-07-11 23:44:01 +0545

    arithmetic operators

---
+++ 02_operators_and_conditionals/01_operators.js
@@ -0,0 +1,19 @@
+// comments in js using double slash
+console.log("Hello World!!");
+/* console.log("Hello World!!"); */
+
+// Arithmetic Operators
+let a = 5;
+let b = 3;
+console.log("Aritmetic operators :");
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);  // Modulus
+console.log("a ** b = ", a**b); // Exponentiation
+
+// Unary Operators
+console.log("Unary operators");
+console.log("++a = ", ++a);
+console.log("--b = ",--b);
