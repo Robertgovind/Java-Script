014a74a2d98c4b853a522dae6ff0061bb6626a74
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-07-16 21:00:30 +0545

    assignment and comaprison operators

--- 02_operators_and_conditionals/01_operators.js
+++ 02_operators_and_conditionals/01_operators.js
@@ -17,3 +17,30 @@ console.log("a ** b = ", a**b); // Exponentiation
 console.log("Unary operators");
 console.log("++a = ", ++a);
 console.log("--b = ",--b);
+
+// Assignment operators
+console.log("Assignment operators : ");
+console.log("a += 2", a += 2);
+console.log("a -= 2", a -= 2);
+console.log("a *= 2", a *= 2);
+console.log("a /= 2", a /= 2);
+console.log("a %= 2", a %= 2);
+console.log("a **= 3", a **= 3);
+
+// Coparison operators
+console.log("Comparison operators :");
+console.log("a == b", a == b);
+console.log("a != b",a != b);
+console.log("a === b", a === b);  // isEqual and type same
+console.log("'5' === '5'", '5' === '5');
+console.log("a > b", a > b);
+console.log("a >= b", a >= b);
+console.log("a < b", a < b);
+console.log("a <= b", a <= b);
+
+
+// Logical operators
+console.log("Logical operators");
+console.log("a>b && b>a", a>b && b>a );
+console.log("a>b || b>a", a>b || b>a);
+console.log("!(a>b)", !(a>b));
